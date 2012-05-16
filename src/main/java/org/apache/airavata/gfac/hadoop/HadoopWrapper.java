2f4476757d6d319440abf2b181e3654ae296c932
Milinda Pathirage <milinda@milinda-OptiPlex-755.(none)>
2012-05-16 16:41:26 -0400

    Initial code.

---
+++ src/main/java/org/apache/airavata/gfac/hadoop/HadoopWrapper.java
@@ -0,0 +1,11 @@
+package org.apache.airavata.gfac.hadoop;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: milinda
+ * Date: 5/16/12
+ * Time: 3:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HadoopWrapper {
+}
